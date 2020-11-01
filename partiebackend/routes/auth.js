cbb7b8a1afd66e8a7a126b54cc3f571284dd76e6
Wassim Khadraoui <wassim.khadraoui@ynov.com>
2020-11-01 02:43:47 +0100

    partie front

--- partiebackend/routes/auth.js
+++ partiebackend/routes/auth.js
@@ -31,7 +31,7 @@ router.post(
     [
       // validation
       check('name', 'Name is required').not().isEmpty(),
-      check('email', ).isEmail(),
+      check('email','bcvnvbn' ).isEmail(),
       check(
         'password',
         'Please enter a password with 6 or more characters'
