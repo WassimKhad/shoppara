d4cb1c331fa992892608e8ff3a05be5e1543844e
Wassim Khadraoui <wassim.khadraoui@ynov.com>
2020-11-01 03:04:32 +0100

    partie front

---
+++ partiefront/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import {Provider} from 'react-redux'
+import store from './data/store'
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
