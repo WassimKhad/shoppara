47b774dc99811775a810a46b76abaf8e5a1f249d
Wassim Khadraoui <wassim.khadraoui@ynov.com>
2020-11-01 13:48:08 +0100

    redux

--- partiefront/src/index.js
+++ partiefront/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/main.css'
+
 
 import App from './App';
 import {Provider} from 'react-redux'
 import store from './data/store'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar/navbar.component.jsx'
 ReactDOM.render(
   <Provider store={store}>
+
+  <BrowserRouter>
+  <Navbar/>
+    <Switch>
+      <Route exact path='/'components={App}/>
+    </Switch>
+  </BrowserRouter>
     <App />
   </Provider>,
   document.getElementById('root')
